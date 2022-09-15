56db0c063dc6e0da38f7a71e99bef4202ed847b6
Zephaniah Ong <zealorez@gmail.com>
2022-09-15 12:59:24 +0800

    Restructure files

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../db/client";
+import { prisma } from "../../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query.slug;
-  console.log(slug);
 
   if (!slug || typeof slug !== "string") {
     res.status(404).send(JSON.stringify({ message: "Please use with aslug" }));
@@ -24,5 +23,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  return res.redirect(data.url);
+  return res.json(data);
 };
