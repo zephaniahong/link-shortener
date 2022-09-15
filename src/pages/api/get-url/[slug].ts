fbf166b2ffee5bd0138e94aecbc192ce2eedf260
Zephaniah Ong <zealorez@gmail.com>
2022-09-15 13:18:49 +0800

    Add response headers

--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -5,6 +5,10 @@ import { prisma } from "../../../db/client";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query.slug;
 
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "s-maxage=100000, stale-while-revalidate");
+
   if (!slug || typeof slug !== "string") {
     res.status(404).send(JSON.stringify({ message: "Please use with aslug" }));
     return;
